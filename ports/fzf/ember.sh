3bf2992ebcb6af956643536f3ee937ab87296600
KyanJeuring <kyanjeuring@gmail.com>
2026-09-15 21:02:17 +0200

    Added fzf port

---
+++ ports/fzf/ember.sh
@@ -0,0 +1,23 @@
+# ==================================================
+# Ember - fzf
+# ==================================================
+
+export FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS:-}
+--color=fg:#C9C3BC
+--color=fg+:#F2EEE9
+--color=bg:#0B0B0B
+--color=bg+:#1C1C1C
+--color=hl:#FF7500
+--color=hl+:#FF8A2B
+--color=border:#FF7500
+--color=label:#FF7500
+--color=prompt:#FF7500
+--color=pointer:#FF7500
+--color=marker:#FF7500
+--color=spinner:#FF7500
+--color=header:#FF7500
+--color=info:#7F766E
+--color=separator:#252525
+--color=scrollbar:#C95C00
+--color=query:#F2EEE9
+"
